2f78e14c7bae39aec1cf78241170cca182ddf72b
Marcelo Mollinari <mmollina@gmail.com>
2018-09-17 15:17:45 -0400

    update: documentation

--- R/est_map_hmm.R
+++ R/est_map_hmm.R
@@ -235,7 +235,7 @@ est_rf_hmm <- function(input.seq, input.ph = NULL,
 #'     
 #' @param thres.hmm the threshold used to determine if the linkage
 #'     phases compared via hmm analysis should be evaluated in the 
-#'     next round of marker inclusion.  
+#'     next round of marker inclusion.   
 #'
 #' @param info.tail if \code{TRUE} uses the complete informative tail
 #'     of the chain (i.e. ploidy x 2 homologous can be distinguished) 
