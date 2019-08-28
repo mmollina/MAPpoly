168a4d3b49cd833513ad9b86327097c6fc925a20
Marcelo Mollinari <mmollina@gmail.com>
2019-08-28 11:59:34 -0400

    Include G. Gesteira as a contributor

--- test.R
+++ test.R
@@ -1,19 +1,58 @@
-data(hexafake)
-mrk.subset<-make_seq_mappoly(hexafake, 1:200)
-red.mrk<-elim_redundant(mrk.subset)
-unique.mrks<-make_seq_mappoly(red.mrk)
-counts.web<-cache_counts_twopt(unique.mrks, get.from.web = TRUE)
-subset.pairs<-est_pairwise_rf(input.seq = unique.mrks,
-                              count.cache = counts.web)
-subset.map <- est_rf_hmm_sequential(input.seq = unique.mrks,
+# Install and restart current MAPpoly branch
+rm(list = ls())
+require(mappoly)
+
+# So far, this dataset is private, however it will be released 
+# as soon as https://doi.org/10.1101/689638 is accepted
+
+# Loading ch12., I. trifida reference
+dat.ch12 <- read_geno_dist(file.in = "~/repos/BT_map/src/genotype_calling/trifida/BT_trifida_12", prob.thres = 0.8)
+match(dat.ch12$ind.names, unique(dat.ch12$geno$ind))
+print(dat.ch12, detailed = TRUE)
+plot(dat.ch12)
+dat.ch12.1 <- filter_missing(input.data = dat.ch12, type = "marker", filter.thres = .4, inter = FALSE)
+match(dat.ch12.1$ind.names, unique(dat.ch12.1$geno$ind))
+dat.ch12.2 <- filter_missing(input.data = dat.ch12.1, type = "individual", filter.thres = .33, inter = FALSE)
+match(dat.ch12.2$ind.names, unique(dat.ch12.2$geno$ind))
+
+print(dat.ch12.2, detailed = TRUE)
+plot(dat.ch12.2)
+seq.chi.ch12.2 <- filter_segregation(input.data = dat.ch12.2, chisq.pval.thres = 5e-6, inter = FALSE)
+seq.chi.ch12.2 <- make_seq_mappoly(dat.ch12.2, seq.chi.ch12.2$keep)
+print(seq.chi.ch12.2, detailed = TRUE)
+plot(seq.chi.ch12.2)
+red.mrk <- elim_redundant(seq.chi.ch12.2)
+plot(red.mrk)
+seq.ch12.2 <- make_seq_mappoly(red.mrk)
+counts <- cache_counts_twopt(seq.ch12.2, get.from.web = TRUE)
+twopt.pairs <- est_pairwise_rf(input.seq = seq.ch12.2, 
+                               n.clusters = 14,
+                               count.cache = counts)
+mat<-rf_list_to_matrix(twopt.pairs, n.clusters = 8)
+plot(mat, ord = rownames(get_genomic_order(seq.ch12.2)))
+w<-rf_snp_filter(input.twopt = twopt.pairs, 
+                 thresh.LOD.ph = 5, 
+                 thresh.LOD.rf = 5, 
+                 thresh.perc = 0.1)
+plot(w)
+mat1<-make_mat_mappoly(input.mat = mat, input.seq = w)
+plot(mat1, ord = rownames(get_genomic_order(w)))
+
+x1 <- make_seq_mappoly(input.obj = dat.ch12., w$seq.mrk.names[1:10])
+tpt1 <- make_pairs_mappoly(input.twopt = twopt.pairs, input.seq = x1)
+
+
+subset.map <- est_rf_hmm_sequential(input.seq = x1,
+                                    start.set = 3,
                                     thres.twopt = 5,
                                     thres.hmm = 10,
                                     extend.tail = 10,
                                     tol = 0.1,
                                     tol.final = 10e-3,
-                                    twopt = subset.pairs,
+                                    twopt = tpt1,
                                     verbose = TRUE,
                                     high.prec = FALSE)
+bla<-est_full_hmm_with_global_error(input.map = subset.map, error = 0.1, tol = 10e-3, th.prob = 0.95)  
 
 s1 <- make_seq_mappoly(hexafake, subset.map$maps[[1]]$seq.num[1:50])                                     
 map1 <- get_submap(subset.map, 1:50)
