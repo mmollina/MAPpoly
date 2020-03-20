eb36b87f3b235cecab91b715790d282ba6be2443
Marcelo Mollinari <mmollina@gmail.com>
2020-03-20 17:51:51 -0400

    updates: two-point analysis

--- R/utils.R
+++ R/utils.R
@@ -39,6 +39,32 @@ get_w_m <- function(m){
    1/factorial((m/2)) * prod(choose(seq(2, m, 2),2))
 }
 
+
+#' Get used memory (unix only)
+#'
+#' @param void interfunction to be documented
+#' @keywords internal
+#' @export
+get_memory<-function(){
+  z<-strsplit(system("free --mega", intern = T)[2], " ")
+  mem<-as.numeric(z[[1]][z[[1]]!=""][3])
+  t.all<-Sys.time()
+  i<-0
+  while(1==1){
+    Sys.sleep(1)
+    z<-strsplit(system("free --mega", intern = T)[2], " ")
+    mem<-c(mem, as.numeric(z[[1]][z[[1]]!=""][3]))
+    pdf("memory_use.pdf", width = 14, height = 8)
+    plot(mem = cumsum(rep(.5, length(mem))), mem, type = "l", lwd = 2, col = 2, xlab = "time (s)", ylab = "memory (MB)")  
+    dev.off()
+    t.all<-c(t.all, Sys.time())
+    save(mem, t.all, file = "memory_use.RData")
+  }
+  return(list(mem = mem, t.all = t.all))
+}
+
+
+
 #' Reverse map
 #'
 #' Provides the reverse version of a given map.
